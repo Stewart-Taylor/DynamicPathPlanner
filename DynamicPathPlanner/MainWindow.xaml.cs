cf35fad5f222d48b53241422e5de13cfbce590d9
Stewart Taylor <stewart@taylore.net>
2013-02-27 22:09:06 +0000

    New Interface
    Removed the old interface.

--- DynamicPathPlanner/MainWindow.xaml.cs
+++ DynamicPathPlanner/MainWindow.xaml.cs
@@ -22,6 +22,34 @@ namespace DynamicPathPlanner
         public MainWindow()
         {
             InitializeComponent();
+
+          //  grid_layout.Visibility = Visibility.Hidden;
+            grid_pangu.Visibility = Visibility.Hidden;
+
+        }
+
+        private void btn_connect_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+          //  grid_layout.Visibility = Visibility.Visible;
+          //  grid_pangu.Visibility = Visibility.Hidden;
+
+            System.Windows.Media.Animation.Storyboard storyBoard = (System.Windows.Media.Animation.Storyboard)FindResource("PanguSlideOut");
+            storyBoard.Completed += new EventHandler(storyBoard_Completed);
+            BeginStoryboard(storyBoard);
+        }
+
+
+
+        void storyBoard_Completed(object sender, EventArgs e)
+        {
+            grid_pangu.Visibility = Visibility.Hidden;
+            grid_layout.Visibility = Visibility.Visible;
+
+            System.Windows.Media.Animation.Storyboard storyBoard = (System.Windows.Media.Animation.Storyboard)FindResource("MainSlideIn");
+            
+            BeginStoryboard(storyBoard);
+ 
         }
     }
 }
