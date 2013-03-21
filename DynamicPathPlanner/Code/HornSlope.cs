e5873d8f4080be9f5634d2940feed988e891fcac
Stewart Taylor <stewart.taylor1@gmail.com>
2013-03-21 14:39:25 +0000

    Git Fix

---
+++ DynamicPathPlanner/Code/HornSlope.cs
@@ -0,0 +1,20 @@
+﻿/*      HornSlope Class
+ *	    AUTHOR: STEWART TAYLOR
+ *------------------------------------
+ * This class is used to generate a slope model 
+ * It uses the Horn Slope method
+ *
+ * Last Updated: 28/02/2013
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamicPathPlanner.Code
+{
+    class HornSlope
+    {
+    }
+}
---
+++ DynamicPathPlanner/Code/MaxSlope.cs
@@ -0,0 +1,21 @@
+﻿/*      MaxSlope Class
+ *	    AUTHOR: STEWART TAYLOR
+ *------------------------------------
+ * This class is used to generate a slope model 
+ * It uses the max method
+ * This means that the highest absolute value will be used
+ *
+ * Last Updated: 28/02/2013
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamicPathPlanner.Code
+{
+    class MaxSlope
+    {
+    }
+}
