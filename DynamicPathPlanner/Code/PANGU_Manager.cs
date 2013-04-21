3c2b16f9b37478097de5d0c5aee3e97996696dd1
Stewart Taylor <stewart.taylor1@gmail.com>
2013-04-21 20:24:48 +0100

    Refactored PANGU_Manager

--- DynamicPathPlanner/Code/PANGU_Manager.cs
+++ DynamicPathPlanner/Code/PANGU_Manager.cs
@@ -4,7 +4,7 @@
  * This class is used as an easy way to simply fetch data from a PANGU server
  * The PANGU Connector contains the wrapper functionality 
  *
- * Last Updated: 16/03/2013
+ * Last Updated: 21/04/2013
 */
 
 using System;
@@ -25,41 +25,28 @@ namespace DynamicPathPlanner.Code
 
         private static PANGU_Connector connector = new PANGU_Connector();
 
-        public static bool connect(String hostname, int port)
-        {
-            if (connectionExists == true)
-            {
-                return true;
-            }
 
-            if (connector.connect(hostname, port) == true)
-            {
-                connectionExists = true;
-                //interfaceManager.addLogEntry("Connected");
-                return true;
-            }
-            return false;
-        }
-
-
-        //Best method name ever!
-        public static void killPANGU()
-        {
-            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
-            {
-                if (myProc.ProcessName == panguProcessName)
-                {
-                    myProc.Kill();
-                    connectionExists = false;
-                }
-            }
-        }
+        #region GET
 
         public static double[,] getElevationModel(float distance, int width, int height)
         {
-            double[,] elevationModel = connector.getDEM(distance, width, height);
+            return connector.getDEM(distance, width, height);
+        }
 
-            return elevationModel;
+        public static Bitmap getImageView(int x, int y, int z, float yaw, float pitch, float roll)
+        {
+            return connector.getImage(x, y, z, yaw, pitch, roll, 30.0f);
+        }
+
+        public static Bitmap getImageView(int x, int y, int z, float yaw, float pitch, float roll, float fov)
+        {
+            return connector.getImage(x, y, z, yaw, pitch, roll, fov);
+        }
+
+
+        public static float getPointHeight(int x, int y)
+        {
+            return connector.getHeightPoint(x, y);
         }
 
         public static ImageSource getSkyView(float distance, int size)
@@ -76,29 +63,6 @@ namespace DynamicPathPlanner.Code
             return bi;
         }
 
-
-        public static Bitmap getImageView(int x, int y , int z , float yaw ,float pitch ,float roll  )
-        {
-            Bitmap bitmap = connector.getImage(x, y, z, yaw, pitch, roll , 30.0f);
-
-            return bitmap;
-        }
-
-        public static Bitmap getImageView(int x, int y, int z, float yaw, float pitch, float roll, float fov )
-        {
-            Bitmap bitmap = connector.getImage(x, y, z, yaw, pitch, roll, fov);
-
-            return bitmap;
-        }
-
-
-        public static float getPointHeight(int x , int y)
-        {
-            float value = connector.getHeightPoint(x, y);
-            return value;
-        }
-
-
         public static Bitmap getSkyBitmap(float distance, int size)
         {
             float x = 0;
@@ -114,9 +78,37 @@ namespace DynamicPathPlanner.Code
             height = (float)size / (2f * (float)Math.Tan(fov / 2f));
             z = height * 0.1f; // Convert to unit distance from pixel distance
 
-            Bitmap skyBitmap = connector.getImage(x, y, z, yaw, pitch, roll ,30.0f);
+            return connector.getImage(x, y, z, yaw, pitch, roll, 30.0f);
+        }
 
-            return skyBitmap;
+        #endregion
+
+        public static bool connect(String hostname, int port)
+        {
+            if (connectionExists == true)
+            {
+                return true;
+            }
+
+            if (connector.connect(hostname, port) == true)
+            {
+                connectionExists = true;
+                interfaceManager.addLogEntry("Connected");
+                return true;
+            }
+            return false;
+        }
+
+        public static void killPANGU()
+        {
+            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == panguProcessName)
+                {
+                    myProc.Kill();
+                    connectionExists = false;
+                }
+            }
         }
 
         public static void endConnection()
